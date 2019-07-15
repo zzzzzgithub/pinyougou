e5c8445aaa93dd32bbc39f41cea28333b2b17f6a
zlc <13537768500@163.com>
2019-07-15 15:39:01 +0800

    运营商后台管理商户审核完善,商户登录权限控制完善

--- pinyougou_parent/pinyougou_manager_web/src/main/java/com/pinyougou/manager/controller/SellerController.java
+++ pinyougou_parent/pinyougou_manager_web/src/main/java/com/pinyougou/manager/controller/SellerController.java
@@ -10,93 +10,102 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+
 /**
  * 请求处理器
- * @author Steven
  *
+ * @author Steven
  */
 @RestController
 @RequestMapping("/seller")
 public class SellerController {
 
-	@Reference
-	private SellerService sellerService;
-	
-	/**
-	 * 返回全部列表
-	 * @return
-	 */
-	@RequestMapping("/findAll")
-	public List<TbSeller> findAll(){			
-		return sellerService.findAll();
-	}
-	
-	
-	/**
-	 * 分页查询数据
-	 * @return
-	 */
-	@RequestMapping("/findPage")
-	public PageResult  findPage(int pageNo,int pageSize,@RequestBody TbSeller seller){			
-		return sellerService.findPage(pageNo, pageSize,seller);
-	}
-	
-	/**
-	 * 增加
-	 * @param seller
-	 * @return
-	 */
-	@RequestMapping("/add")
-	public Result add(@RequestBody TbSeller seller){
-		try {
-			sellerService.add(seller);
-			return new Result(true, "增加成功");
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new Result(false, "增加失败");
-		}
-	}
-	
-	/**
-	 * 修改
-	 * @param seller
-	 * @return
-	 */
-	@RequestMapping("/update")
-	public Result update(@RequestBody TbSeller seller){
-		try {
-			sellerService.update(seller);
-			return new Result(true, "修改成功");
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new Result(false, "修改失败");
-		}
-	}	
-	
-	/**
-	 * 获取实体
-	 * @param id
-	 * @return
-	 */
-	@RequestMapping("/getById")
-	public TbSeller getById(String id){
-		return sellerService.getById(id);		
-	}
-	
-	/**
-	 * 批量删除
-	 * @param ids
-	 * @return
-	 */
-	@RequestMapping("/delete")
-	public Result delete(Long [] ids){
-		try {
-			sellerService.delete(ids);
-			return new Result(true, "删除成功"); 
-		} catch (Exception e) {
-			e.printStackTrace();
-			return new Result(false, "删除失败");
-		}
-	}
-	
+    @Reference
+    private SellerService sellerService;
+
+    /**
+     * 返回全部列表
+     *
+     * @return
+     */
+    @RequestMapping("/findAll")
+    public List<TbSeller> findAll() {
+        return sellerService.findAll();
+    }
+
+
+    /**
+     * 分页查询数据
+     *
+     * @return
+     */
+    @RequestMapping("/findPage")
+    public PageResult findPage(int pageNo, int pageSize, @RequestBody TbSeller seller) {
+        return sellerService.findPage(pageNo, pageSize, seller);
+    }
+
+    /**
+     * 增加
+     *
+     * @param seller
+     * @return
+     */
+    @RequestMapping("/add")
+    public Result add(@RequestBody TbSeller seller) {
+
+
+        try {
+            sellerService.add(seller);
+            return new Result(true, "增加成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false, "增加失败");
+        }
+    }
+
+    /**
+     * 修改
+     *
+     * @param seller
+     * @return
+     */
+    @RequestMapping("/update")
+    public Result update(@RequestBody TbSeller seller) {
+        try {
+            sellerService.update(seller);
+            return new Result(true, "修改成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false, "修改失败");
+        }
+    }
+
+    /**
+     * 获取实体
+     *
+     * @param id
+     * @return
+     */
+    @RequestMapping("/getById")
+    public TbSeller getById(String id) {
+        return sellerService.getById(id);
+    }
+
+    /**
+     * 批量删除
+     *
+     * @param ids
+     * @return
+     */
+    @RequestMapping("/delete")
+    public Result delete(Long[] ids) {
+        try {
+            sellerService.delete(ids);
+            return new Result(true, "删除成功");
+        } catch (Exception e) {
+            e.printStackTrace();
+            return new Result(false, "删除失败");
+        }
+    }
+
 }
--- pinyougou_parent/pinyougou_shop_web/src/main/java/com/pinyougou/shop/service/UserDetailsServiceImpl.java
+++ pinyougou_parent/pinyougou_shop_web/src/main/java/com/pinyougou/shop/service/UserDetailsServiceImpl.java
@@ -13,29 +13,34 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 
-/***
- * 扩展权限认证类
+/**
+ * 扩展SpringSecurity认证方式
+ * @author Steven
+ * @version 1.0
+ * @description com.pinyougou.shop.service
+ * @date 2019-7-14
  */
 public class UserDetailsServiceImpl implements UserDetailsService {
 
     @Reference
     private SellerService sellerService;
 
-
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        System.out.println(username + ",进入了loadUserByUsername方法");
-        //构造用户的角色列表
+        System.out.println(username + "进入方法loadUserByUsername....");
+        //设置角色列表
         List<GrantedAuthority> authorities = new ArrayList<>();
         authorities.add(new SimpleGrantedAuthority("ROLE_SELLER"));
 
+        //查询商家信息
         TbSeller seller = sellerService.getById(username);
-        //如果查找到相关商家信息冰企鹅商家为已审核状态
-        if (seller != null && "1".equals(seller.getStatus())) {
-            //返回真实存在的用户名,让security框架对配置用户与密码信息是否匹配
-            return new User(username, "123456", authorities);
-        } else {
-            return null;
+        if(seller != null){
+            //只有已审核通过的商家才能登录
+            if("1".equals(seller.getStatus())){
+                return new User(username,seller.getPassword(),authorities);
+            }
         }
+        //返回null对象，告诉SpringSecurity认证失败
+        return null;
     }
 }
